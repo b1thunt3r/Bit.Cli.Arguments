99da5f83b38cd08b15a92cf7fbdb7f0e31df304e
Ishan Jain <ishan@jain.se>
2015-09-02 20:18:33 +0200

    Initial

---
+++ Bit.Console.Arguments/Options.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bit.Console.Arguments
+{
+    public class Options : ICollection<Option>
+    {
+    }
+
+    public class Option
+    {
+        public string ShortName { get; set; }
+    }
+}
