9310d39a1c2415f7c0b0922391fa5f30b17f2f51
Ishan Jain <ishan@jain.se>
2015-09-06 16:26:18 +0200

    Start adding some code.

--- Bit.Console.Arguments/Options.cs
+++ Bit.Console.Arguments/Options.cs
@@ -1,13 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Bit.Console.Arguments
 {
     public class Options : ICollection<Option>
     {
+        public void Add(Option item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(Option item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(Option[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public bool Remove(Option item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<Option> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
     public class Option
     {
         public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public Action<string> Action { get; set; }
+        public string Description { get; set; }
     }
 }
