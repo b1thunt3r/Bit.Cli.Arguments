a7224b32bbf4758cc5037df2a67bab6c54ce7242
Ishan Jain <ishan@jain.se>
2015-09-20 22:24:46 +0200

    Start with help

--- Bit.Cli.Arguments/ExtensionMethods.cs
+++ Bit.Cli.Arguments/ExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Bit.Cli.Arguments
@@ -16,16 +17,43 @@ namespace Bit.Cli.Arguments
             return options.FirstOrDefault(o => o.LongName == arg);
         }
 
+        public static void PrintHelp(this IList<Option> options)
+        {
+            foreach (var option in options)
+            {
+                if (string.IsNullOrWhiteSpace(option.ShortName) && !string.IsNullOrWhiteSpace(option.LongName))
+                {
+                    Console.WriteLine("", option.ShortName);
+                }
+                else if (!string.IsNullOrWhiteSpace(option.ShortName) && string.IsNullOrWhiteSpace(option.LongName))
+                {
+
+                    Console.WriteLine("", option.LongName);
+                }
+                else if (!string.IsNullOrWhiteSpace(option.ShortName) && !string.IsNullOrWhiteSpace(option.LongName))
+                {
+
+                    Console.WriteLine("", option.ShortName, option.LongName);
+                }
+            }
+        }
+
         public static void Execute(this IList<Option> options, string[] args)
         {
             foreach (var s in args)
             {
                 if (s.StartsWith("--"))
                 {
-                    var arg = s.Split("=:".ToCharArray());
+                    var arg = s.Substring(2).Split("=:".ToCharArray());
                     if (arg.Length != 2) continue;
 
-                    var name = options.FindWithLongtName(arg[0].Substring(2));
+                    if (arg[0].ToUpper().Equals("HELP"))
+                    {
+                        options.PrintHelp();
+                        break;
+                    }
+
+                    var name = options.FindWithLongtName(arg[0]);
                     if (name != null) ;
                 }
                 else if (s.StartsWith("-"))
@@ -35,7 +63,7 @@ namespace Bit.Cli.Arguments
                 }
                 else if (s.StartsWith("/"))
                 {
-
+                    //?
                 }
                 else
                 {
